891f0b869607a95227c5c164d0720843918d8e4a
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-03 17:16:11 +0500

    massage

--- text.js
+++ text.js
@@ -1,2 +1,10 @@
 const currentDate = new Date();
 console.log(currentDate);
+let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  console.log(message);
+}
+showMessage();
+
