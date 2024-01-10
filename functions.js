f8347764dba387812bc3756d693986e20b9f0c77
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-10 20:42:17 +0500

    new

--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
 function user(){
     // console.log("Say Hello");
 }
-user()
-
+// user()
 
 function addTwoNumbers (number1,number2){
-    console.log(number1+number2);
+    // console.log(number1+number2);
 }
-addTwoNumbers(11,4)
+// addTwoNumbers(11,4)
+(function tea(){
+    console.log(`MongoDB`);
+})();
+
+( (name) => {
+    console.log(`MERN`);
+})()
