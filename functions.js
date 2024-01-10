b024b94e1a90c5b0fb6d77e84b533bbafd1f898d
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-10 16:29:19 +0500

    new

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
 function user(){
-    console.log("Say Hello");
+    // console.log("Say Hello");
 }
 user()
+
+
+function addTwoNumbers (number1,number2){
+    console.log(number1+number2);
+}
+addTwoNumbers(11,4)
