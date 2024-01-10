d8e7b929a1ab994e97c9d395ae1418ae797d25b6
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-10 15:48:12 +0500

    name

---
+++ object.js
@@ -0,0 +1,36 @@
+const create ={
+    name:"Tayyab",
+    age:18,
+    location:"Masnehra",
+    email:"mansehra@gmail.com ",
+    isLoggedIn:false,
+    LastLoginDays:["Monday","tuesday"]
+}
+// console.log(create.name);
+const mallta= {}
+
+mallta.id="123"
+mallta.name="tayyab"
+mallta.isLoggedIn=false
+// console.log(mallta);
+
+
+const redularUser = {
+    email:"hitesh@google.com",
+    fullName:{
+        userfullname:{
+            firstname:"Hi",
+            lastname:"i am here"
+        }
+    }
+}
+// const {email:E}= redularUser
+// console.log(E); is used for the object destructure method
+// console.log(redularUser.fullName.userfullname.firstname);
+
+let obj1={1:"0",2:"3"}
+let obj2={3:"B",4:"C"}
+let obj3={obj1,obj2}
+// console.log(obj3);
+
+
