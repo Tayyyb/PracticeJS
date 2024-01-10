464c3e757bb3e8f18f85be226562fdd6623f670c
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-10 13:57:49 +0500

    new code

--- objest.js
+++ objest.js
@@ -6,4 +6,28 @@ const create ={
     isLoggedIn:false,
     LastLoginDays:["Monday","tuesday"]
 }
-console.log(create.name);
+// console.log(create.name);
+const mallta= {}
+
+mallta.id="123"
+mallta.name="tayyab"
+mallta.isLoggedIn=false
+// console.log(mallta);
+
+
+const redularUser = {
+    email:"hitesh@google.com",
+    fullName:{
+        userfullname:{
+            firstname:"Hi",
+            lastname:"i am here"
+        }
+    }
+}
+// console.log(redularUser.fullName.userfullname.firstname);
+
+let obj1={1:"0",2:"3"}
+let obj2={3:"B",4:"C"}
+let obj3={obj1,obj2}
+console.log(obj3);
+
