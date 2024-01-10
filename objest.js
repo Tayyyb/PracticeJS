2140bf8c008499d0b2d898a39c21db127f045483
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-10 12:55:03 +0500

    new file

---
+++ objest.js
@@ -0,0 +1,9 @@
+const create ={
+    name:"Tayyab",
+    age:18,
+    location:"Masnehra",
+    email:"mansehra@gmail.com ",
+    isLoggedIn:false,
+    LastLoginDays:["Monday","tuesday"]
+}
+console.log(create.name);
