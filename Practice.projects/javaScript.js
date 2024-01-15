960ada8d38b94b704f335c7ba7e25f39923c25f9
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-15 17:19:56 +0500

    new

---
+++ Practice.projects/javaScript.js
@@ -0,0 +1,12 @@
+// main.forEach(function (main) {
+    
+// console.log(main);
+    
+// });
+
+
+const display=document.querySelector(".div1");
+const body=document.querySelector("body");
+display.addEventListener("click",function(){
+    body.style.backgroundColor="grey";
+});
