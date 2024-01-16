61f71fa4f34c258bf9195ac577263cd620493bc1
Tayyab Zaheer <106226625+Tayyyb@users.noreply.github.com>
2024-01-16 16:33:08 +0500

    new

--- Practice.projects/javaScript.js
+++ Practice.projects/javaScript.js
@@ -1,12 +1,29 @@
-// main.forEach(function (main) {
-    
-// console.log(main);
-    
-// });
 
-
-const display=document.querySelector(".div1");
-const body=document.querySelector("body");
-display.addEventListener("click",function(){
-    body.style.backgroundColor="grey";
+const btn=document.querySelectorAll(".button")
+const heading=document.querySelector(".heading")
+const body=document.querySelector("body")
+heading.addEventListener("click", function(e){
+    if(e.target.id=== "black"){
+        body.style.backgroundColor = e.target.id;
+    }
 });
+btn.forEach((btn)=>{
+   btn.addEventListener("click", function(e){
+    if(e.target.id=== "Yellow"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id=== "red"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id=== "blue"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id=== "grey"){
+        body.style.backgroundColor = e.target.id;
+    }
+   })
+});
+document.getElementById('white').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
+
